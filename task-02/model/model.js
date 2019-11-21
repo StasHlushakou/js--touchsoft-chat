f402fe2642821135131be3c40603310350aca38d
Stanislau Hlushakou <stas.hlushakou@gmail.com>
2019-11-22 00:14:18 +0300

    task-2 initial

---
+++ task-02/model/model.js
@@ -0,0 +1,78 @@
+if (sessionStorage.getItem('isMinimize') == null){
+	sessionStorage.setItem('isMinimize', true);
+} else{
+	if (sessionStorage.getItem('isMinimize') == "false"){
+		minimizeButton();
+	} 
+}
+
+if (sessionStorage.getItem('messages') == null){
+	sessionStorage.setItem('messages', "");
+} else{
+	messageOutput.value = sessionStorage.getItem('messages');
+}
+
+function minimizeButton() {
+	chatMinimize.hidden =  !chatMinimize.hidden;
+	minimizeBtn.value = minimizeBtn.value == "-" ? "[]" : "-";
+	sessionStorage.setItem('isMinimize', chatMinimize.hidden);
+}
+
+function sendButton() {
+	let msg = message.value;
+    if (msg != ""){
+    	message.value = "";
+    	sendToServer(msg);
+    	writeToMessageOutput("Вы", msg);
+    }
+}
+
+function writeToMessageOutput(from, message) {
+    let date = new Date();
+    messageOutput.value += date.getHours() + ":" + date.getMinutes() + " " + from + " : " + message + "\r\n";
+    sessionStorage.setItem('messages', messageOutput.value);
+
+}
+/*
+
+ 	Для использования fetch() вместо XHR раскомментировать
+ 	async в описании метода sendToServer() и код метода заменить на тот,
+ 	который в комментарии.
+ */
+
+/*async*/ function sendToServer(str) {
+	
+	/*
+	let msg = {};
+	msg.text = str;
+	let url = `http://localhost:8080/chatJS`;
+	let response = await fetch(url,{
+  		method: 'POST',
+  		headers: {
+    		'Content-Type': 'application/json;charset=utf-8'
+  		},
+  		body: JSON.stringify(msg)
+	});
+	if (response.ok) { 
+	  let json = await response.json();
+	  writeToMessageOutput("Bot",json.text);
+	} else {
+	  console.log("Ошибка HTTP: " + response.status);
+	}
+	*/
+	
+	let msg = {};
+	msg.text = str;
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", `http://localhost:8080/chatJS`)
+	xhr.setRequestHeader('Content-Type', 'application/json;charset=utf-8');
+	xhr.send(JSON.stringify(msg));
+	xhr.onload = function() {
+	  	let json = JSON.parse(xhr.response);
+	 	writeToMessageOutput("Bot", json.text);
+	};
+	xhr.onerror = function() { 
+	  	console.log(`Ошибка соединения`);
+	};
+	
+}
