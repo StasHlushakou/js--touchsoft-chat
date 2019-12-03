1a406c92d94437ee3a7bd0ec5a7055223d9ea8ac
Stanislau Hlushakou <stas.hlushakou@gmail.com>
2019-12-04 00:53:03 +0300

    refactor task-01 from new server

--- task-01/chatScript/chat.js
+++ task-01/chatScript/chat.js
@@ -1,21 +1,42 @@
 init();
 
-// обработка нажатия кнопки сворачивания
+
+ 
+
+function Message (text, readUser, readAdmin){
+	let date = new Date();
+    this.text = text;
+    this.userID = chatLS.userid;
+    this.readUser = readUser;
+	this.readAdmin = readAdmin;
+    this.senderName = chatLS.userName;
+    this.time = (date.getHours().toString().length > 1 ? date.getHours() : "0" + date.getHours()) + ":" + 
+    (date.getMinutes().toString().length > 1 ? date.getMinutes() : "0" +date.getMinutes()); 
+}
+
+
+/**
+ * Handles clicking a minimize button.
+ */
 function minimizeButton() {
     chatMinimize.hidden =  !chatMinimize.hidden;
     minimizeBtn.value = minimizeBtn.value == "-" ? "[]" : "-";
-    sessionStorage.setItem('isHidden', chatMinimize.hidden);
+    chatLS.isHidden = chatMinimize.hidden;
+    sessionStorage.chatLS = JSON.stringify(chatLS);
 }
 
-// обработка нажатия кнопки отправки сообщения
+/**
+ * Handles the push of a message send button
+ */
 function sendButton() {
     // проверка ввода имени пользователя и его регистрация, если требуется
-    if (sessionStorage.getItem('userName') == ""){
+    if (!chatLS.userName){
         if (message.value != ""){
-            sessionStorage.setItem('userName', message.value);
+            chatLS.userName = message.value;
+            sessionStorage.chatLS = JSON.stringify(chatLS);
             message.value = "";
-            regNewUser(sessionStorage.getItem('userName'));
-            addServiceMessage("Your name is " + sessionStorage.getItem('userName'));
+            regNewUser(chatLS.userName);
+            addServiceMessage("Your name is " + chatLS.userName);
             return;
         } else {
             return;
@@ -23,50 +44,61 @@ function sendButton() {
     }
     // обработка вводимых сообщений
     if (message.value != ""){
-        let msg = {};
-	    msg.userid = sessionStorage.getItem('userid');
-	    msg.username = sessionStorage.getItem('userName')
-	    let date = new Date();
-	    msg.time = date.getHours() + ":" + date.getMinutes();
-	    msg.text = message.value;
-		msg.read = true;
+        let msg = new Message(message.value, true, false);
         sendMessageToServer(msg);
         writeToMessageOutput(msg);
         message.value = "";
     }
 }
 
-function setOnlineStatus(user){
-	requestToServer("POST", TSChat.chatURL + "/users/online", user, null);
+/**
+ * Handles the push of a message send button
+ */
+window.onunload = function() {
+  setOnlineUserStatus(TSChat.user, false, true)
+};
+
+
+
+
+/**
+ * Sends a message to the server to set the user status online / offline.
+ * @param {string} user - The user whose status is to be set.
+ * @param {boolean} status - Status online/offline.
+ * @param {boolean} keepalive - Run a query in the background after the user leaves the page.
+ */
+function setOnlineUserStatus(user, status, keepalive){
+	user.online = status;
+	requestToServer("POST", TSChat.chatURL + "/users", user, null, keepalive);
 }
 
 
-window.onunload = function() {
-  TSChat.user.online = false;
-  requestToServer("POST", TSChat.chatURL + "/users/online", TSChat.user, null, true);
-};
 
-// запись в поле вывода служебной информации
+/**
+ * Output service information in the output field.
+ * @param {string} str - String from output.
+ */
 function addServiceMessage(str){
-
-	messageOutput.value += str + "\r\n";
-	sessionStorage.setItem('messages', messageOutput.value);
+	messageOutput.value += str + "\n";
+	chatLS.messages = messageOutput.value;
+	sessionStorage.chatLS = JSON.stringify(chatLS);
 }
 
 function setReadMessageStatus(message){
-	message.read = true;
+	message.readUser = true;
 	sendMessageToServer(message);
 }
 
 // запись в поле вывода сообщения
 function writeToMessageOutput(message) {
     if (TSChat.showTime){
-        messageOutput.value += message.time + " " + message.username + ":" + message.text + "\r\n";
+        messageOutput.value += message.time + " " + message.senderName + ":" + message.text + "\n";
     } else{
-        messageOutput.value += message.username + ":" + message.text + "\r\n";
+        messageOutput.value += message.senderName + ":" + message.text + "\n";
     }
-    sessionStorage.setItem('messages', messageOutput.value);
-    if (message.read == false){
+    chatLS.messages = messageOutput.value;
+    sessionStorage.chatLS = JSON.stringify(chatLS);
+    if (!message.readUser){
     	setReadMessageStatus(message);       	
     }
 }
@@ -80,16 +112,17 @@ function addHistoryMessages(messagesArr){
 // Загружает историю сообщений переданного пользователя
 function dounloadHistoryMessages(user){
 	
-	requestToServer("POST", TSChat.chatURL + "/messages/users", user, addHistoryMessages);
+	requestToServer("GET", TSChat.chatURL + "/messages/users/" + user.id, null, addHistoryMessages);
 }
 
 // Устанавливает id пользователя, полученного с сервера и запускает загрузку его сообщений
 function setUser(user){
 	TSChat.user = user;
-	sessionStorage.setItem('userid', user.id);
+	chatLS.userid = user.id;
+	sessionStorage.chatLS = JSON.stringify(chatLS);
 	dounloadHistoryMessages(user);
 	TSChat.user.online = true;
-	setOnlineStatus(TSChat.user);
+	setOnlineUserStatus(TSChat.user);
 }
 
 // Регистрирует нового пользователя или получает данные старого по его имени с сервера
@@ -97,7 +130,7 @@ function regNewUser(username){
 	let user = {};
 	user.name = username;
 	user.botname = TSChat.botName;
-	requestToServer("POST", TSChat.chatURL + "/users", user, setUser);
+	requestToServer("POST", TSChat.chatURL + "/users/reg", user, setUser);
 }
 
 // Отправить сообщение на сервер
@@ -107,11 +140,11 @@ function sendMessageToServer(message){
 }
 
 // реализация отправки запроса на сервер
-function requestToServer(method, url, json, func) {    
+function requestToServer(method, url, json, func, keepalive) {    
     if (TSChat.connectType == "xhr"){
-    	xhrRequestToServer(method, url, json, func);
+    	xhrRequestToServer(method, url, json, func, keepalive);
     }else {
-    	fetchRequestToServer(method, url, json, func)
+    	fetchRequestToServer(method, url, json, func, keepalive)
     }
 }
 
@@ -172,10 +205,12 @@ async function fetchRequestToServer(method, url, json, func, keepalive) {
 }
 
 function updateMessages(){
-	let user = {};
-	user.id = sessionStorage.getItem('userid');
-	requestToServer("POST", TSChat.chatURL + "/messages/users/unread", user, addHistoryMessages);
+	if (chatLS.userName){
+		requestToServer("GET", TSChat.chatURL + "/messages/users/userunread/" + chatLS.userid, null, addHistoryMessages);
+	}
 }
+
+
 
 /*
 function updateCommands(){
@@ -214,9 +249,7 @@ function init(){
 	        <input id="btnSend" type="button" value="Send" onclick="sendButton()">
 	    </div>
 	</div>`;
-
 	document.body.append(newDiv);
-
 	function loadCss(href) {
 	    var link = document.createElement("link");
 	    link.rel = "stylesheet";
@@ -224,8 +257,6 @@ function init(){
 	    document.head.appendChild(link);
 	}
 	loadCss("chat.css");
-
-
 	// установка заголовка чата
 	heading.textContent = TSChat.title;
 	
@@ -234,6 +265,43 @@ function init(){
 	    chatWindow.style.left="5px";
 	}
 
+	// загрузка сохранённых данных из локального хранилища
+	if (!sessionStorage.getItem('chatLS')){
+		chatLS = {};
+		chatLS.isHidden = true;
+		//проверка требования ввода имени
+		if (TSChat.requireName){
+	        chatLS.userName = "";
+	        addServiceMessage("Please, enter your name.");
+	    } else {
+	        chatLS.userName = "Вы";
+	        regNewUser(chatLS.userName);
+	        addServiceMessage("Your name is " + chatLS.userName);
+	    }
+	    chatLS.messages = "";
+	    if(TSChat.allowDrag){
+	    	chatLS.dragLeft = 'no';
+	        chatLS.dragTop = 'no';
+	    }
+
+		sessionStorage.chatLS = JSON.stringify(chatLS);
+    } else{
+        chatLS = sessionStorage.chatLS ? JSON.parse(sessionStorage.chatLS) : [];
+        messageOutput.value = chatLS.messages;
+		
+		if(TSChat.allowDrag){
+			if (chatLS.dragLeft != 'no'){
+	            chatWindow.style.position = 'fixed';
+	            chatWindow.style.bottom = "auto";
+	            chatWindow.style.right = "auto";
+	            chatWindow.style.left = chatLS.dragLeft;
+	            chatWindow.style.top = chatLS.dragTop;
+	        }
+		}
+
+
+    }
+
 
 	// проверка разрешения сворачивания чата и установка чату 
 	// необходимого состояния после перезагрузки страницы
@@ -241,14 +309,8 @@ function init(){
 	    chatMinimize.hidden = false;
 	    minimizeBtn.disabled = true; 
 	    minimizeBtn.value = "-"
-	}else{
-	    if (sessionStorage.getItem('isHidden') == null){
-	        sessionStorage.setItem('isHidden', true);
-	    } else{
-	        if (sessionStorage.getItem('isHidden') == "false"){
-	            minimizeButton();
-	        } 
-	    }
+	}else if (!chatLS.isHidden){
+        minimizeButton();
 	}
 
 	// установка перетаскивания, если разрешено
@@ -272,8 +334,9 @@ function init(){
 	        chatWindow.onmouseup = function() {
 	            document.removeEventListener('mousemove', onMouseMove);
 	            chatWindow.onmouseup = null;
-	            sessionStorage.setItem('dragLeft', chatWindow.style.left); 
-	            sessionStorage.setItem('dragTop', chatWindow.style.top);
+	            chatLS.dragLeft = chatWindow.style.left; 
+	            chatLS.dragTop = chatWindow.style.top;
+	            sessionStorage.chatLS = JSON.stringify(chatLS);
 
 	        };
 	    };
@@ -281,41 +344,6 @@ function init(){
 	      return false;
 	    };
 	}
-
-	// просьба ввести имя, если оно не установлено
-	if (sessionStorage.getItem('userName') == null){
-	    if (TSChat.requireName){
-	        sessionStorage.setItem('userName', "");
-	        addServiceMessage("Please, enter your name.");
-	    } else {
-	        sessionStorage.setItem('userName', "Вы");
-	        regNewUser(sessionStorage.getItem('userName'));
-	        addServiceMessage("Your name is " + sessionStorage.getItem('userName'));
-	    }
-	}		
-
-	// заполнение формы вывода после перезагрузки страницы
-	if (sessionStorage.getItem('messages') == null){
-	    sessionStorage.setItem('messages', "");
-	} else{
-	    messageOutput.value = sessionStorage.getItem('messages');
-	}
-
-	//установка нужного положения при обновлении страницы
-	if(TSChat.allowDrag){
-	    if (sessionStorage.getItem('dragLeft') == null){
-	        sessionStorage.setItem('dragLeft', 'no');
-	        sessionStorage.setItem('dragTop', 'no');
-	    } else{
-	        if (sessionStorage.getItem('dragLeft') != 'no'){
-	            chatWindow.style.position = 'fixed';
-	            chatWindow.style.bottom = "auto";
-	            chatWindow.style.right = "auto";
-	            chatWindow.style.left = sessionStorage.getItem('dragLeft');
-	            chatWindow.style.top = sessionStorage.getItem('dragTop');
-	        }
-	    }
-	}	
 
 	let msgUpdate = setInterval(updateMessages, 1000);
 	//let commandUpdate = setInterval(updateCommands, 1000);
