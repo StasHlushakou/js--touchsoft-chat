9da1f494f1c7c4e916bac06ffe5b15ec61175af8
Stanislau Hlushakou <stas.hlushakou@gmail.com>
2019-12-07 14:39:27 +0300

    Task SPA is completed.

--- SPA/spa.js
+++ SPA/spa.js
@@ -1,8 +1,29 @@
-// При загрузке страницы - считать состояние и запустить обработчик
 handleUrl(window.location.hash);
 
+function viewDashboard(){
+    let url = "dashboard";
+    window.location.hash = url;
+	dashboard.hidden = false;
+    chatConfigurator.hidden = true;
+    about.hidden = true;
+}
 
-// Создать обработчик URL
+function viewConfiguration(){
+    let url = "config";
+    window.location.hash = url;
+    dashboard.hidden = true;
+    chatConfigurator.hidden = false;
+    about.hidden = true;
+}
+
+function viewAbout(){
+    let url = "about";
+    window.location.hash = url;
+    dashboard.hidden = true;
+    chatConfigurator.hidden = true;
+    about.hidden = false;
+}
+
 function handleUrl(url) {
     if (url == "#config"){
         viewConfiguration();
@@ -12,60 +33,3 @@ function handleUrl(url) {
         viewAbout();
     }
 }
-
-
-function viewConfiguration(){
-    let url = "config";
-    window.location.hash = url;
-
-    console.log("config");
-}
-
-function viewDashboard(){
-    let url = "dashboard";
-    window.location.hash = url;
-
-    console.log("dashboard");
-}
-
-function viewAbout(){
-    let url = "about";
-    window.location.hash = url;
-
-    console.log("about");
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//var counter = document.querySelector('#counter');
-//var content = document.querySelector('#content');
-//setInterval(() => counter.innerText = new Date(), 1000);
-
-
-// Подписаться на изменения URL
-//window.addEventListener('hashchange', (ev) => handleUrl(ev.newURL));
-
-/*
-// Переопределить поведение внутренних ссылок
-document.body.addEventListener('click', (ev) => {
-    if(!ev.target.matches('a')) {
-        return;
-    }
-    ev.preventDefault();
-
-    // При клике по ссылке - обновлять URL
-    let url = ev.target.getAttribute('href');
-    window.location.hash = url;
-});
-*/
